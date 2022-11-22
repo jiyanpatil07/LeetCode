2a44c18c9a6b8c7f710ef15e32337621cf3104df
Jiyan Patil <jiyanpatil0707@gmail.com>
2022-11-22 10:36:00 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (74.14%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+              int start=0;
+        int end=nums.length-1;
+        while (start<end){
+            int middle=start+(end-start)/2;
+       if(nums[middle]<nums[middle+1]){
+           start=middle+1;
+       }
+            else{
+            end=middle;
+            }
+        
+       
+    }
+         return start; 
+    }
+}
