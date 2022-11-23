aee1e25f445e642c24318d65bae566f554dcdd68
Jiyan Patil <jiyanpatil0707@gmail.com>
2022-11-23 10:51:06 +0530

    Time: 9 ms (97.48%), Space: 41.4 MB (95.04%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int r=0;
+        int sum=0;
+        int temp=x;
+        while(x>0){
+            r=x%10;
+            sum=(sum*10)+r;
+            x=x/10;
+            
+        }
+        if(sum==temp){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+}
