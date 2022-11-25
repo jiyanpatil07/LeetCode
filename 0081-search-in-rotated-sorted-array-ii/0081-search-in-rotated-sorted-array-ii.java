047cf71848618f997283bdc3391f71451093e016
Jiyan Patil <jiyanpatil0707@gmail.com>
2022-11-25 08:54:00 +0530

    Time: 1 ms (84.49%), Space: 44.6 MB (5.01%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+   public boolean search(int[] nums, int target) {
+                int left=0;
+        int right = nums.length-1;
+        while(left <= right){
+            int mid = left + (right -left)/2;
+            if(nums[mid] == target) return true;
+            if(nums[mid] > nums[left]){ 
+                if(nums[left] <= target && target < nums[mid]){
+                    right = mid-1;
+                }else{
+                    left = mid +1;
+                }
+            }else if (nums[mid] < nums[left]){
+                if(nums[mid] <= target && target <= nums[right]){
+                    left = mid +1;
+                }else{
+                    right = mid -1;
+                }
+            }else{
+                left++;// move a step ahead. cause  there is no gaurantee the target will show on which part. cause the shap of first part[left, mid] is not decided.
+            }
+        }
+        return false;
+    }
+}
